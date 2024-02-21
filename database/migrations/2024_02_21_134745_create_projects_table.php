53d58ad6376b2be3c66c16b3d91c33c911f420f4
gds90 <you@example.com>
2024-02-21 15:03:56 +0100

    Added projects table schema

--- database/migrations/2024_02_21_134745_create_projects_table.php
+++ database/migrations/2024_02_21_134745_create_projects_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('description');
+            $table->string('link', 150);
+            $table->string('slug', 100);
             $table->timestamps();
         });
     }
