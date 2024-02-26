814de179ff445804845c4b1eba311bd7916764d1
gds90 <you@example.com>
2024-02-26 14:53:33 +0100

    Added img in show.blade.php, with a placeholder img if cover_image = null

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -4,7 +4,16 @@
     <div class="container">
         <div class="row">
             <div class="col-8 m-auto my-5 bg-light p-5 ">
+                <div class="text-center mb-3">
+                    @if ($project->cover_image !== null)
+                        <img src="{{ asset('storage') . '/' . $project->cover_image }}" alt="{{ $project->title }}"
+                            width="75%">
+                    @else
+                        <img src="{{ asset('/img/test-img.jpg') }}" alt="{{ $project->title }}">
+                    @endif
+                </div>
                 <h2>Titolo: {{ $project->title }}</h2>
+
                 <p>Descrizione: {{ $project->description }}</p>
                 <p>Link: {{ $project->link }}</p>
                 <div class="">
